bc6fe4a4915a8dbc5b8344e12ee96e2e3185325c
Luciana <eng.luciana.cabral@gmail.com>
2020-11-05 09:57:47 -0300

    First commit

---
+++ src/main/java/br/edu/usj/ads/lgll/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package br.edu.usj.ads.lgll.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
